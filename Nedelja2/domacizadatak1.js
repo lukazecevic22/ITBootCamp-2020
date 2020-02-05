4e29ba8c70ba8f692e41611c963988d9f322115d
Luka Zecevic <lukazecevic22@yahoo.com>
2020-02-05 17:16:53 +0100

    domacizad2

---
+++ Nedelja2/domacizadatak1.js
@@ -0,0 +1,13 @@
+// zadatak1.js:
+// 1. За два броја (а и б) исписати њихов: Збир,разлику,производ,
+// количник,остатак при дељењу,степен (а^b)
+
+let a = 7;
+let b = 6;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(Math.pow(a,b));
