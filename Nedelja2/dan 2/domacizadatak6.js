570d5e77eef1d0bef10785cdbd5cad0eb5917eec
Luka Zecevic <lukazecevic22@yahoo.com>
2020-02-05 19:10:28 +0100

    zadaci

---
+++ Nedelja2/dan 2/domacizadatak6.js
@@ -0,0 +1,15 @@
+// drugi dan
+
+
+// zadatak4.js
+
+//     Исписати првих 1000 бројева дељивих са 5
+
+
+
+let nmbr = 1000;
+for(var i = 1; i <= 1001; i++)
+    if( i % 5 == 0){
+        console.log(i)
+    }
+
