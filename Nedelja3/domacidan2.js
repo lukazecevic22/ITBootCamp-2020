7e9c5cbffaee4f03046bb8fddcddd223066716dd
Luka Zecevic <lukazecevic22@yahoo.com>
2020-02-12 16:24:04 +0100

    izmena

--- Nedelja3/domacidan2.js
+++ Nedelja3/domacidan2.js
@@ -1,11 +1,21 @@
-let r='32'
-let cena='150'
+//PRVI NACIN
 
-function PricePizza(r){
-    return (`${(r**2 *Math.PI)}`);
+// let r='32'
+// let cena='150'
 
-}
-let info=PricePizza(r);
-console.log(`cena pizze po cm2 je ${info/cena} RSD`);
+// function PricePizza(r){
+//     return (`${(r**2 *Math.PI)}`);
+
+// }
+// let info=PricePizza(r);
+// console.log(`cena pizze po cm2 je ${cena/info} RSD`);
 
 
+//DRUGI NACIN
+
+// function pizza(r, cena){
+//     P=r**2*Math.PI
+//     cm=cena/P
+//     }
+//     pizza(32, 150);
+//     console.log(cm)
