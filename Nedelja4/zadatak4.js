b19257f788dfc70e0508d9a55698d564f0db9a47
Luka Zecevic <lukazecevic22@yahoo.com>
2020-02-26 16:29:21 +0100

    5zad

--- Nedelja4/zadatak4.js
+++ Nedelja4/zadatak4.js
@@ -27,29 +27,18 @@ characteristics: {attack: 84,defence:78, speed:100}
 //Направити функцију која прима низ покемона, 
 //пореди покемоне по јачини и враћа као победника оног који има највећу јачину напада.
 
-let pokemoni = [pokemon1,pokemon2,pokemon3,pokemon4];
+let pokemoni = [pokemon1, pokemon2, pokemon3, pokemon4];
 
-// function SviPokemoni(pikacu){
-//     for(i=0; i < pikacu.length; i++){
-//         if(pikacu[i+1] >pikacu[i]);
-//         }
-//       return `${pikacu.characteristics.attack}`
-// }
-// console.log(SviPokemoni(pokemoni));
+function sviPokemoni(pokemoni) {
+    let max = pokemoni[0];
+    
+    for (let i = 1; i < pokemoni.length; i++) {
+        if (max.characteristics.attack < pokemoni[i].characteristics.attack) {
+            max = pokemoni[i];
+        }
+    }
 
-// let pola=pokemoni.map((element)=>{
-//     for(i=0; i<element.length; i++)
-//     if(element[i+1]>element[i]){
-//         return element.characteristics.attack
-//     }
-// })
-// console.log(pola);
-
-let pokus=pokemoni.find((osoba)=>{
-    for(i=0; i < osoba.length; i++){
-    if(osoba[i].characteristics.defence >osoba[i].characteristics.defence);
- }
- return osoba
-})
-console.log(pokus);
+    return max;
+}
+console.log(sviPokemoni(pokemoni));
 
