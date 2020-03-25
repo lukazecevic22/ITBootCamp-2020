4f36d3ba91734411ea6a9346d8956de793b24760
lukazecevic22 <60389166+lukazecevic22@users.noreply.github.com>
2020-03-25 18:51:31 +0100

    Update index.js

--- Nedelja6/src/index.js
+++ Nedelja6/src/index.js
@@ -1,13 +1,26 @@
 import LaunchList from "./components/launch_list";
 import Header from "./components/header";
+import selectLaunchYear from "./components/selecetor";
 
-const app=document.querySelector('#app');
+const app = document.querySelector('#app');
+const header1 = Header()
+header1.style.color = "white"
+const header = document.createElement('header');
 
-const header1=Header()
-const header=document.createElement('header');
-const filters=document.createElement('section');
-const launch_list=LaunchList();
+const filters = document.createElement('section');
+const launch_list = LaunchList();
+const select = selectLaunchYear()
+const wrapper = document.createElement('div')
 
-const footer=document.createElement('footer');
+filters.appendChild(select)
+filters.append(wrapper)
 
-app.append(header1,header,filters,launch_list,footer);
+const footer = document.createElement('footer');
+footer.innerHTML = `by IT Bootcamp`
+footer.style.color = "white"
+
+
+app.append(header1, header, filters, launch_list, footer);
+
+export { filters, wrapper }
+
